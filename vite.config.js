19a255fe3d0190d92146afc9881c593dd090cfa5
Alok-2003 <alok953280@gmail.com>
2025-03-31 19:51:16 +0530

    chore: comment out server allowedHosts configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['4c80-2409-40d5-100f-803a-8b64-ff42-4119-d24.ngrok-free.app/', 'localhost', '127.0.0.1']
-  }
+  // server: {
+  //   allowedHosts: ['4c80-2409-40d5-100f-803a-8b64-ff42-4119-d24.ngrok-free.app/', 'localhost', '127.0.0.1']
+  // }
 })
